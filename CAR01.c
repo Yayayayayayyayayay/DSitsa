7d4b4da9c9dd75e2f7cd302a1c335ea745555b1d
Yayayayayayyayayay <167853802+Yayayayayayyayayay@users.noreply.github.com>
2025-01-11 17:40:17 +0800

    Add files via upload

--- CAR01.c
+++ CAR01.c
@@ -2,13 +2,13 @@
 
 int main() {
     int arr[100], n = 0;
-    while (scanf("%d", &arr[n]) != EOF) {
+    while (scanf("%d", &arr[n]) != EOF) {//輸入陣列
         n++;
     }
     for (int i = n - 1; i >= 0; i--) {
-        printf("%d", arr[i]);
+        printf("%d", arr[i]); //從尾端開始輸出陣列
         if (i > 0) {
-            printf(" ");
+            printf(" ");//不到尾端輸出就空格
         }
     }
     printf("\n");
